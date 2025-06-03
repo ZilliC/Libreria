35a9c89a08173672bc3a908a3a75b3aa3bf68a19
ZilliC <33742812+ZilliC@users.noreply.github.com>
2025-06-03 01:35:05 -0600

    same

--- Libreria/main.c
+++ Libreria/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
     printf("Firstcommitpush¿?\n");
     printf("whaddafuckisgoingoooooon\n");
     printf("howboutthisbiatch\n");
