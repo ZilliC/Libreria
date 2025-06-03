00d99f9070c1043f71a3c92e87162660bedb3528
ZilliC <33742812+ZilliC@users.noreply.github.com>
2025-06-03 01:19:29 -0600

    Initial Commit

---
+++ Libreria/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Libreria
+//
+//  Created by Carlos Zilli Montero on 03/06/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
